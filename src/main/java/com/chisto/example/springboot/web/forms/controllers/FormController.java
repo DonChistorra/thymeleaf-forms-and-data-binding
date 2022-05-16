a134bbc13da2c530ed729d20ec672f30b968d245
Don Chistorra <shadowshaoran@gmail.com>
2022-05-16 16:05:15 +0200

    Añadida dependencia Spring Validation

---
+++ src/main/java/com/chisto/example/springboot/web/forms/controllers/FormController.java
@@ -0,0 +1,20 @@
+package com.chisto.example.springboot.web.forms.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller("/web")
+public class FormController {
+
+	@GetMapping("/form")
+	public String form(Model model) {
+		return "form";
+	}
+	
+	@PostMapping("/form")
+	public String procesarForm(Model model) {
+		return "processedForm";
+	}
+}
