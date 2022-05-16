8e90f3c1ce431eb5360c3c020a7784bf1e487cf7
Don Chistorra <shadowshaoran@gmail.com>
2022-05-16 14:36:35 +0200

    Commit Inicial

---
+++ src/main/java/com/chisto/example/springboot/web/forms/CursospringFormsDatabindingApplication.java
@@ -0,0 +1,13 @@
+package com.chisto.example.springboot.web.forms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursospringFormsDatabindingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursospringFormsDatabindingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chisto/example/springboot/web/forms/CursospringFormsDatabindingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chisto.example.springboot.web.forms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringFormsDatabindingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
